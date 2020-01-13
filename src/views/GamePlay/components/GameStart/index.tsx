47edbcf090faaeaafe8dd2059f0fff4e3dbffd7e
Đặng Đình Khánh <khanhdd@appota.com>
2020-01-13 10:48:29 +0700

    add btn history lixi

--- src/views/GamePlay/components/GameStart/index.tsx
+++ src/views/GamePlay/components/GameStart/index.tsx
@@ -3,18 +3,26 @@ import React from 'react';
 const GameStart: React.FC<{ ready?: boolean }> = ({ ready = true }) => {
 	return (
 		<div className="animated easily bounceIn lixi ">
-			<img
-				className="animated easily"
-				src={ready ? require('src/image/molixxi.png') : require('src/image/tet.png')}
-			/>
+			{!ready && (
+				<div className="logoTet">
+					<div className="logoTet_Inner">
+						<img className="logoTet_Bg" src={require('src/image/tet_bg.png')} alt=""/>
+						<img className="logoTet_Circle" src={require('src/image/tet_circle.png')} alt=""/>
+					</div>
+				</div>
+			)}
+			{ready && (
+				<img
+					className="animated easily"
+					src={require('src/image/molixxi.png')}
+				/>
+			)}
+			
 			{ready && (
 				<>
 					<div className="animated easily bounceIn btn_lac">
 						<img className="" src={require('src/image/btn-lac.png')} alt="" />
 					</div>
-					{/* <div className="laclac">
-				<img className="tada" src={require('src/image/laclac.png')} alt="" />
-			</div> */}
 					<div className="light-top">
 						<img src={require('src/image/light-top.png')} />
 					</div>
